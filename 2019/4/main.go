f43a910efdc503a72c75efd75c2415ec1c0fd603
Erin Stanfill <erin@truss.works>
2019-12-04 01:40:59 -0500

    day 4 part 2

--- 2019/4/main.go
+++ 2019/4/main.go
@@ -7,11 +7,17 @@ import (
 
 func main() {
 	countForPartOne := 0
+	countForPartTwo := 0
 	for i :=240298; i <=784956; i++{
-		if lib.IsPossiblePassword(i) {
+		a, b := lib.IsPossiblePassword(i) 
+		if a {
 			countForPartOne++
+		}
+		if b {
+			countForPartTwo++
 		}
 
 	}
-	fmt.Println("part 1: ", countForPartOne)
+	fmt.Println("part 1: ", countForPartOne) //1150
+	fmt.Println("part 2: ", countForPartTwo) //748
 }
